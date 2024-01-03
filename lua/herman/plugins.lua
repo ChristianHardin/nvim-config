34f9c0b6e279ad6ae2b505f23ba6e8308eed9478
ChristianHardin <christianhardin12345@gmail.com>
2024-01-03 15:53:52 -0500

    feat: added autopairs

--- lua/herman/plugins.lua
+++ lua/herman/plugins.lua
@@ -1,55 +1,60 @@
 require("lazy").setup({
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
 
-	{
-		'nvim-lualine/lualine.nvim',
-		-- dependencies = { 'nvim-tree/nvim-web-devicons' }
-	},
+    {
+        'nvim-lualine/lualine.nvim',
+        -- dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
 
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate"
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
 
-	{
-		'ThePrimeagen/harpoon',
-		branch = 'harpoon2',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
+    {
+        'ThePrimeagen/harpoon',
+        branch = 'harpoon2',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 
-	{'mbbill/undotree'},
-	{'tpope/vim-fugitive'},
+    {'mbbill/undotree'},
+    {'tpope/vim-fugitive'},
 
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		lazy = true,
-		config = false,
-	},
-	{
-		'neovim/nvim-lspconfig',
-		dependencies = {
-			{'hrsh7th/cmp-nvim-lsp'},
-		}
-	},
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        lazy = true,
+        config = false,
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+        }
+    },
 
-	{
-		'hrsh7th/nvim-cmp',
-		dependencies = {
-			{'L3MON4D3/LuaSnip'}
-		},
-	},
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            {'L3MON4D3/LuaSnip'}
+        },
+    },
 
-	{"williamboman/mason.nvim"},
-	{'williamboman/mason-lspconfig.nvim'},
+    {"williamboman/mason.nvim"},
+    {'williamboman/mason-lspconfig.nvim'},
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {} -- this is equalent to setup({}) function
+    }
 })
